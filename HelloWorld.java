50bf981100421bd403898542e23ae3e761c9ed38
Rajeev Loghade <rajeevloghade010@gmail.com>
2021-06-10 17:38:05 +0530

    code changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld{
 		for(int i=1;i<=10;i++){
 		System.out.println("Hello world : "+i);	
 		}
+		for(int i=10;i>0;i--){
+		System.out.println("Hello world 2: "+i);	
+		}
 	}
 }
